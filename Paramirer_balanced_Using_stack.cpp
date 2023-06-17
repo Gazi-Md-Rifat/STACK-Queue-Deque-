4e12d8b93d38fb690b7747095f2d4d5c9de62422
Sharp Thinker <133127411+Sharp-Thinker@users.noreply.github.com>
2023-06-17 16:06:25 +0600

    Update Paramirer_balanced_Using_stack.cpp

--- Paramirer_balanced_Using_stack.cpp
+++ Paramirer_balanced_Using_stack.cpp
@@ -4,71 +4,72 @@ using namespace std;
 bool check_balance(string s)
 {
     stack<char> data;
-        for (int i = 0; i < s.length(); i++)
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
         {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            data.push(s[i]);
+        }
+        else 
+        {
+            switch (s[i])
             {
-                data.push(s[i]);
-            }
-            else 
-            {
-                switch (s[i])
+            case ')':
+                if (data.empty())
                 {
-                case ')':
-                    if (data.empty())
-                    {
-                        return false;
-                    }
-                    if (data.top() == '(')
-                    {
-                        data.pop();
-                    }
-                    else 
-                    {
-                        return false;
-                    }
-                    break;
-                case '}':
-                    if (data.empty())
-                    {
-                        return false;
-                    }
-                    if (data.top() == '{')
-                    {
-                        data.pop();
-                    }
-                    else 
-                    {
-                        return false;
-                    }
-                    break;
-                case ']':
-                    if (data.empty())
-                    {
-                        return false;
-                    }
-                    if (data.top() == '[')
-                    {
-                        data.pop();
-                    }
-                    else 
-                    {
-                        return false;
-                    }
-                    break;
+                    return false;
                 }
+                if (data.top() == '(')
+                {
+                    data.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+                break;
+            case '}':
+                if (data.empty())
+                {
+                    return false;
+                }
+                if (data.top() == '{')
+                {
+                    data.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+                break;
+            case ']':
+                if (data.empty())
+                {
+                    return false;
+                }
+                if (data.top() == '[')
+                {
+                    data.pop();
+                }
+                else 
+                {
+                    return false;
+                }
+                break;
             }
         }
-        if (data.empty())
-        {
-            return true;
-        }
-        return false;
+    }
+    if (data.empty())
+    {
+        return true;
+    }
+    return false;
 }
 
 int main()
 {
     string paramiter = {"({[([{}])]})"};
+    // paramiter = {"()[]}{"};
     bool result = check_balance(paramiter);
     if (result)
     {
